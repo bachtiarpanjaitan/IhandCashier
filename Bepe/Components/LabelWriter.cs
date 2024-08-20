77ceb8648a65a60957b2b718975726be014e1931
Bachtiar Panjaitan <bachtiarpanjaitan0@gmail.com>
2024-08-20 14:35:34 +0700

    Perbaikan Logger dan Info.plist

--- Bepe/Components/LabelWriter.cs
+++ Bepe/Components/LabelWriter.cs
@@ -7,40 +7,25 @@ using Microsoft.Maui.Controls;
 public class LabelWriter : TextWriter
 {
     private readonly Label _label;
-    private readonly StringBuilder _content;
 
     public LabelWriter(Label label)
     {
         _label = label;
-        _content = new StringBuilder();
     }
 
     public override Encoding Encoding => Encoding.UTF8;
 
-    public override void Write(char value)
-    {
-        _content.Append(value);
-        UpdateLabel();
-    }
-
-    public override void Write(string value)
-    {
-        _content.Append(value);
-        UpdateLabel();
-    }
-
     public override void WriteLine(string value)
     {
-        _content.AppendLine(value);
-        UpdateLabel();
+        UpdateLabel(value);
     }
 
-    private void UpdateLabel()
+    private void UpdateLabel(string text)
     {
         // Update Label di UI Thread
         MainThread.BeginInvokeOnMainThread(() =>
         {
-            _label.Text = "Log :: " + _content.ToString();
+            _label.Text = "Log :: " + text;
         });
     }
 }
